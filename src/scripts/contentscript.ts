cb6fe17104efcb7352369eed08f3490539596077
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-04-15 00:59:49 -0700

    bug fix

--- src/scripts/contentscript.ts
+++ src/scripts/contentscript.ts
@@ -1,17 +1,18 @@
 const baiduQuery: string = 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=';
 const baiduIcon: string = 'images/icons/48.png';
 const closeButtonIcon: string = 'images/close-button.png';
+let searchTerm: string = '';
 
 // This should be done in the manifest file :(
 const excludedPages = ['https://www.google.ca/?gws_rd=ssl', 'https://www.google.ca',
-'https://www.google.com/?gws_rd=ssl', 'https://www.google.com'];
+    'https://www.google.com/?gws_rd=ssl', 'https://www.google.com'];
 
 let isLoaded: boolean = false;
 
 function inject_icon(): void {
 
     let timer = function (): void {
-        setTimeout(function(): void {
+        setTimeout(function (): void {
             if (excludedPages.indexOf(window.location.href) === -1) {
                 let icon: string = '';
                 icon += '<a href="javascript:void(0)" id="injected-button" class="baidu-icon"></a>';
@@ -37,17 +38,31 @@ function inject_icon(): void {
     timer();
 }
 
-function create_iframe(): void {
+function show_iframe(): void {
+    let searchQuery: string = $('input.gsfi').val();
 
-    if (isLoaded) {
-        $('.baidu-iframe').show();
-    }
+    // Grey out Google background
+    $('html').css({ 'overflow-y': 'hidden' });
+    $('#viewport').css({ 'filter': 'grayscale(1)' });
 
-    // Toggle button
+    // Toggle button class
     $('#injected-button').css({ 'background': 'url(' + chrome.extension.getURL(closeButtonIcon) + ') no-repeat' });
     $('#injected-button').toggleClass('close-icon baidu-icon');
+
+    if (isLoaded && searchTerm === searchQuery) {
+        $('html').css({ 'overflow-y': 'hidden' });
+        $('.baidu-iframe').show();
+    } else {
+        create_iframe(searchQuery);
+    }
+}
+
+function create_iframe(searchQuery: string): void {
     let iframe: string = '';
-    let searchQuery: string = $('input.gsfi').val();
+    if ($('.baidu-iframe').length) {
+        $('.baidu-iframe').remove();
+    }
+
     iframe += '<iframe class="baidu-iframe" src="' + baiduQuery + searchQuery + '"></iframe>';
     $('body').prepend(iframe);
     $('.baidu-iframe').css({
@@ -60,14 +75,14 @@ function create_iframe(): void {
         'border': 'solid'
     });
 
-    // Hide google SERP
-    $('#viewport').css({ 'filter': 'grayscale(1)'});
+    searchTerm = searchQuery;
     isLoaded = true;
 }
 
-function close_iframe(): void {
+function hide_iframe(): void {
     // Bring google back
-    $('#viewport').css({ 'filter': ''});
+    $('#viewport').css({ 'filter': '' });
+    $('html').css({ 'overflow-y': 'auto' });
     $('#injected-button').toggleClass('close-icon baidu-icon');
     $('#injected-button').css({ 'background': 'url(' + chrome.extension.getURL(baiduIcon) + ') no-repeat' });
     $('.baidu-iframe').hide();
@@ -77,9 +92,9 @@ function initButtonListener(): void {
     $('#injected-button').on('click', function (): void {
         event.preventDefault();
         if ($(this).hasClass('close-icon')) {
-            close_iframe();
+            hide_iframe();
         } else {
-            create_iframe();
+            show_iframe();
         }
     });
 }
