0d24ca9261890537724ba92c533b411a7ab7b86f
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-04-14 21:49:23 -0700

    loaded iframe as soon as page loads

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,9 +1,10 @@
 var baiduQuery = 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=';
 var baiduIcon = 'images/icons/48.png';
 var closeButtonIcon = 'images/close-button.png';
+var isLoaded = false;
 function inject_icon() {
     var icon = '';
-    icon += '<button onclick="#" id="injected-button" class="baidu-icon"></button>';
+    icon += '<a href="javascript:void(0)" id="injected-button" class="baidu-icon"></a>';
     $('body').prepend(icon);
     $('#injected-button').css({
         'height': '50px',
@@ -12,47 +13,52 @@ function inject_icon() {
         'border': '1px solid #fff',
         'position': 'fixed',
         'right': '60px',
-        'top': '60px',
+        'top': '70px',
         'z-index': '999',
         'background': 'url(' + chrome.extension.getURL(baiduIcon) + ') no-repeat'
     });
 }
-function create_iframe() {
+function show_iframe() {
     $('#injected-button').css({ 'background': 'url(' + chrome.extension.getURL(closeButtonIcon) + ') no-repeat' });
     $('#injected-button').toggleClass('close-icon baidu-icon');
-    var iframe = '';
-    var searchQuery = $('input.gsfi').val();
-    iframe += '<iframe class="baidu-iframe" src="' + baiduQuery + searchQuery + '"></iframe>';
-    $('body').prepend(iframe);
-    $('.baidu-iframe').css({
-        'position': 'absolute',
-        'z-index': '999',
-        'width': '89.5%',
-        'height': '90%',
-        'top': '37px',
-        'left': '79px',
-        'body': 'solid'
-    });
-    $('#viewport').css({ 'filter': 'grayscale(1)', 'overflow': 'hidden' });
+    $('#viewport').css({ 'filter': 'grayscale(1)' });
+    $('.baidu-iframe').show();
 }
-function close_iframe() {
-    $('#viewport').css({ 'filter': '', 'overflow': 'auto' });
+function hide_iframe() {
+    $('#viewport').css({ 'filter': '' });
     $('#injected-button').toggleClass('close-icon baidu-icon');
     $('#injected-button').css({ 'background': 'url(' + chrome.extension.getURL(baiduIcon) + ') no-repeat' });
     $('.baidu-iframe').hide();
 }
 function initButtonListener() {
-    $('#injected-button').click(function () {
+    $('#injected-button').on('click', function () {
         event.preventDefault();
         if ($(this).hasClass('close-icon')) {
-            create_iframe();
+            hide_iframe();
         }
         else {
-            close_iframe();
+            show_iframe();
         }
     });
 }
-$(document).ready(function () {
+function loadIframe() {
+    var iframe = '';
+    var searchQuery = $('input.gsfi').val();
+    iframe += '<iframe class="baidu-iframe" src="' + baiduQuery + searchQuery + '"></iframe>';
+    $('body').prepend(iframe);
+    $('.baidu-iframe').css({
+        'position': 'fixed',
+        'z-index': '999',
+        'width': '89.5%',
+        'height': '90%',
+        'top': '37px',
+        'left': '79px',
+        'border': 'solid',
+        'display': 'none'
+    });
+}
+$(window).load(function () {
     inject_icon();
     initButtonListener();
+    loadIframe();
 });
--- src/scripts/contentscript.ts
+++ src/scripts/contentscript.ts
@@ -1,10 +1,11 @@
 const baiduQuery: string = 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=';
 const baiduIcon: string = 'images/icons/48.png';
 const closeButtonIcon: string = 'images/close-button.png';
+let isLoaded: boolean = false;
 
 function inject_icon(): void {
     let icon: string = '';
-    icon += '<button onclick="#" id="injected-button" class="baidu-icon"></button>';
+    icon += '<a href="javascript:void(0)" id="injected-button" class="baidu-icon"></a>';
     $('body').prepend(icon);
     $('#injected-button').css({
         'height': '50px',
@@ -13,55 +14,59 @@ function inject_icon(): void {
         'border': '1px solid #fff',
         'position': 'fixed',
         'right': '60px',
-        'top': '60px',
+        'top': '70px',
         'z-index': '999',
         'background': 'url(' + chrome.extension.getURL(baiduIcon) + ') no-repeat'
     });
 }
 
-function create_iframe(): void {
+function show_iframe(): void {
     // Toggle button
     $('#injected-button').css({ 'background': 'url(' + chrome.extension.getURL(closeButtonIcon) + ') no-repeat' });
     $('#injected-button').toggleClass('close-icon baidu-icon');
-    let iframe: string = '';
-    let searchQuery: string = $('input.gsfi').val();
-    iframe += '<iframe class="baidu-iframe" src="' + baiduQuery + searchQuery + '"></iframe>';
-    $('body').prepend(iframe);
-    $('.baidu-iframe').css({
-        'position': 'absolute',
-        'z-index': '999',
-        'width': '89.5%',
-        'height': '90%',
-        'top': '37px',
-        'left': '79px',
-        'body': 'solid'
-    });
-
     // Hide google SERP
-    $('#viewport').css({ 'filter': 'grayscale(1)', 'overflow': 'hidden' });
+    $('#viewport').css({ 'filter': 'grayscale(1)'});
+    $('.baidu-iframe').show();
 }
 
-function close_iframe(): void {
+function hide_iframe(): void {
     // Bring google back
-    $('#viewport').css({ 'filter': '', 'overflow': 'auto' });
+    $('#viewport').css({ 'filter': ''});
     $('#injected-button').toggleClass('close-icon baidu-icon');
     $('#injected-button').css({ 'background': 'url(' + chrome.extension.getURL(baiduIcon) + ') no-repeat' });
     $('.baidu-iframe').hide();
 }
 
 function initButtonListener(): void {
-    $('#injected-button').click(function (): void {
+    $('#injected-button').on('click', function (): void {
         event.preventDefault();
         if ($(this).hasClass('close-icon')) {
-            create_iframe();
+            hide_iframe();
         } else {
-            close_iframe();
+            show_iframe();
         }
-
     });
 }
 
-$(document).ready(function (): void {
+function loadIframe(): void {
+    let iframe: string = '';
+    let searchQuery: string = $('input.gsfi').val();
+    iframe += '<iframe class="baidu-iframe" src="' + baiduQuery + searchQuery + '"></iframe>';
+    $('body').prepend(iframe);
+    $('.baidu-iframe').css({
+        'position': 'fixed',
+        'z-index': '999',
+        'width': '89.5%',
+        'height': '90%',
+        'top': '37px',
+        'left': '79px',
+        'border': 'solid',
+        'display': 'none'
+    });
+}
+
+$(window).load(function (): void {
     inject_icon();
     initButtonListener();
+    loadIframe();
 });
