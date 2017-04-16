a716c198853d2166966f111068f04bf54cc41c92
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-04-15 20:45:47 -0700

    check disable status at beginning

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,4 +1,11 @@
 $(document).ready(function (): void {
+
+    chrome.storage.sync.get('isDisable', function (obj: any): void {
+        if (obj.isDisable === 'True') {
+            $('#option').text('Enable');
+        }
+    });
+
     $('#option').click(function (): void {
         if ($('#option').text() === 'Disable') {
             $('#option').text('Enable');
@@ -7,6 +14,7 @@ $(document).ready(function (): void {
                 chrome.tabs.sendMessage(obj.tabID, {isDisable: 'True'});
             });
         } else {
+            $('#option').text('Disable');
             chrome.storage.sync.set({ 'isDisable': 'False' });
             chrome.storage.sync.get('tabID', function (obj: any): void {
                 chrome.tabs.sendMessage(obj.tabID, {isDisable: 'False'});
